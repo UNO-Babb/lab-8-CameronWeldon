4fcfe911055a297874e890e20f5ee796c2c9bcd9
CameronWeldon <cameronweldon@unomaha.edu>
2025-05-16 21:18:44 +0000

    Done with Lab 8.

--- ProcessData.py
+++ ProcessData.py
@@ -1,23 +1,63 @@
 #ProcessData.py
-#Name:
+#Name: Cameron Weldon
 #Date:
-#Assignment:
+#Assignment: Lab 8
 
 import random
 
 def main():
 
-  #Open the files we will be using
+  # Open
   inFile = open("names.dat", 'r')
   outFile = open("StudentList.csv", 'w')
 
-  #Process each line of the input file and output to the CSV file
+  # Write csv
+  outFile.write("Last Name,First Name,UserID,Major-Year\n")
 
+  # output to csv
+  for line in inFile:
+    data = line.split()
+    first = data[0]
+    last = data[1]
+    idNum = data[3]
+    year = data[5]
+    major = data[6]
 
+    student_id = makeID(first, last, idNum)
 
-  #Close files in the end to save and ensure they are not damaged.
+    # major and year
+    year_abbrev = convertYear(year)
+    major_abbrev = major[:3].upper()
+    major_year = major_abbrev + "-" + year_abbrev
+
+    # Write to CSV
+    output = last + "," + first + "," + student_id + "," + major_year + "\n"
+    outFile.write(output)
+
+  
   inFile.close()
   outFile.close()
+
+
+def makeID(first, last, idNum):
+  idLen = len(idNum)
+
+  while len(last) < 5:
+    last = last + "X"
+
+  id = first[0].lower() + last.lower() + idNum[idLen - 3:]
+  return id
+
+
+def convertYear(year):
+  year_map = {
+    "Freshman": "FR",
+    "Sophomore": "SO",
+    "Junior": "JR",
+    "Senior": "SR"
+  }
+  return year_map.get(year, "UN")  # Use "UN" for unknown
+
 
 if __name__ == '__main__':
   main()
